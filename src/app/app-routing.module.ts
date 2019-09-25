269e3e52d5e4d1568f3b0b2be143a6d237e88564
Giovanny Cerna <gcerna@csumb.edu>
2019-09-24 18:19:21 -0700

    Sprint1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SignComponent } from './sign/sign.component';
+import { ProductsComponent } from './products/products.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: 'home', component: HomeComponent},
+	{ path: 'sign', component: SignComponent},
+	{ path: 'products', component: ProductsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
